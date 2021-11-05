8a5859fab0ab6abcec6fbf5e7ab8bee3af7b0a1c
Brice Romero <39904674+br-novelt@users.noreply.github.com>
2021-11-05 22:03:42 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+  fmt.Println("App starting...")
+}
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func loadFile(filename string) {
+  dat, err := os.ReadFile(filename)
+  check(err)
+  fmt.Print(string(dat))
+}
